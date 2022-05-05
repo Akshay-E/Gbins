b80fad7862c834e28b3bc1dcf3f1e419c9230495
Akshay-E <eakshay0109@gmail.com>
2022-05-05 10:38:03 -0700

    Dispersion by convolution

--- Broad_Band/broad_band.py
+++ Broad_Band/broad_band.py
@@ -3,14 +3,13 @@ import os
 import sys
 import time 
 import shutil 
+from setigen.voltage import raw_utils
 
 try:
     import cupy as xp
 except ImportError:
     import numpy as xp
 import numpy as np
-
-from setigen.voltage import raw_utils
 
 
 class broadband(object):
@@ -24,6 +23,7 @@ class broadband(object):
                 ):
     
         self.raw_params = raw_utils.get_raw_params(input_file_stem=input_file_stem)
+        self.raw_params['fch1'], self.raw_params['chan_bw'], self.raw_params['center_freq'] = self.raw_params['fch1']/1e6, self.raw_params['chan_bw']/1e6, self.raw_params['center_freq']/1e6
         self.obs_bw= self.raw_params['num_chans'] * self.raw_params['chan_bw']
         
         self.input_file_stem=input_file_stem
@@ -34,20 +34,30 @@ class broadband(object):
         self.pulse_time=pulse_time
         self.smear=self.calc_smear() 
         
-        
-        assert self.pulse_time < self.raw_params['obs_length'], 'injection time cannot be greater than length of file'
-        assert self.pulse_time + self.smear<self.raw_params['obs_length'], 'smearing across band exceeds length of file'
-        
-        
+        assert self.pulse_time!=0 , f"injection time cannot be 0"
+        assert self.pulse_time < self.raw_params['obs_length'], f"injection time cannot be greater than length of file( {self.raw_params['obs_length']}) "
+    
         self.blocks_per_file = raw_utils.get_blocks_per_file(input_file_stem)
-
         self.time_per_block = self.raw_params['block_size'] / (self.raw_params['num_antennas'] * self.raw_params['num_chans'] * (2 * self.raw_params['num_pols'] * self.raw_params['num_bits'] // 8)) * self.raw_params['tbin']
         
-        shutil.copyfile(f'{self.input_file_stem}.0000.raw', f'{self.input_file_stem}_dispersed.0000.raw')
+        self.blocks_to_read=int(np.ceil(self.smear/self.time_per_block))
         
-        self.blocks_to_read=np.ceil(self.smear/self.time_per_block)
-        self.start_block=np.floor(self.pulse_time/self.time_per_block)
+        self.start_block= int(np.ceil(self.pulse_time/self.time_per_block))
+        
+#         if self.raw_params['ascending']:
+#             self.adjusted_pulse_time=(self.start_block) * self.time_per_block
+#             assert self.adjusted_pulse_time - self.smear>=0, f"smearing across band({self.smear}) exceeds length of file({self.raw_params['obs_length']}) "
+#         else:
+#             self.adjusted_pulse_time=(self.start_block-1) * self.time_per_block
+#             assert self.adjusted_pulse_time + self.smear<self.raw_params['obs_length'], f"smearing across band({self.smear}) exceeds length of file({self.raw_params['obs_length']}) "
 
+        self.adjusted_pulse_time=(self.start_block) * self.time_per_block - self.time_per_block/2.0
+        assert self.adjusted_pulse_time - self.smear>=0, f"smearing across band({self.smear}) from adjusted pulses time({self.adjusted_pulse_time}) exceeds length of file({self.raw_params['obs_length']}) "
+        assert self.adjusted_pulse_time + self.smear<self.raw_params['obs_length'], f"smearing across band({self.smear}) from adjusted pulses time({self.adjusted_pulse_time}) exceeds length of file({self.raw_params['obs_length']}) "
+        
+        
+        print(f"Adjusted injection time for first channel: {round(self.adjusted_pulse_time,3)}")
+        
         header=raw_utils.read_header(f'{self.input_file_stem}.0000.raw')
         
         self.header_size= int(512 * np.ceil((80 * (len(header) + 1)) / 512))
@@ -55,20 +65,99 @@ class broadband(object):
         self.chan_size=int(self.data_size/self.raw_params['num_chans'])
         self.block_read_size =  self.header_size + self.data_size
         
-        self.sim_len= self.blocks_to_read * self.data_size
+        self.sim_len= int(self.blocks_to_read * self.chan_size/2)
+        shutil.copyfile(f'{self.input_file_stem}.0000.raw', f'{self.input_file_stem}_dispersed.0000.raw')
         
-       
-    def calc_smear(self):
         
-        f_start=self.raw_params['fch1']/1e6
-        f_end=(self.raw_params['fch1'] + self.obs_bw)/1e6
+    def calc_smear(self,x=2):
+        
+        f_start=self.raw_params['fch1']
+        f_end=(self.raw_params['fch1'] + self.obs_bw)
         
         if not self.raw_params['ascending']:
             f_start, f_end = f_end, f_start
         
-        smear= self.D*self.dm*(f_start**-2 - f_end**-2)
+        smear= self.D*self.dm*(f_start**-x - f_end**-x)
         print(smear)
         return(smear)
+    
+    
+    def gaussian(self, x, mu, std, C):
+        
+        denominator = xp.sqrt(2*xp.pi*std**2)
+        numerator = xp.exp(-1*(x - mu)**2/(2*std**2))
+        return C*(numerator/denominator)
+    
+    def simulate_pulse(self, cmplx=True):
+        
+        x = xp.arange(self.width) #Span of Gaussian Pulse
+        gaussian_window1 = self.gaussian(x, int(self.width/4), 50, 10000)
+        gaussian_window2 = self.gaussian(x, int(3*self.width/4), 50, 20000)
+        gaussian_window = 1+ gaussian_window1 + gaussian_window2
+        scale_factors = xp.sqrt(2)*gaussian_window
+        noise = xp.random.normal(0,1,self.sim_len)
+        
+#         if self.raw_params['ascending']:
+#             loc=self.sim_len-2*self.width
+#         else:
+#             loc=2*self.width
+        loc=self.sim_len/2
+            
+        for i in range(self.width):
+            if scale_factors[i] >= 1:
+                noise[(int)(loc) + i] *= scale_factors[i]
+            else:
+                continue
+        
+        if cmplx:
+            final_arr = noise+1j*noise
+        else:
+            final_arr = xp.zeros(2*self.sim_len)
+            final_arr[::2] = noise
+            final_arr[1::2] = noise
+        
+        ax1 = plt.subplot(211)
+        ax1.plot(xp.asnumpy(gaussian_window))
+        ax2 = plt.subplot(212)
+        ax2.plot(xp.asnumpy(final_arr.real))
+        plt.show()
+        
+        return final_arr
+    
+    
+    def _write_(self,chan_data, chan_no, reverse=False):
+         
+        if reverse:
+            chan_no= self.raw_params['num_chans'] - 1 - chan_no
+        
+        print(f"writing chan {chan_no}")
+        
+        
+#         chan_data.reshape(self.blocks_to_read, self.chan_size)
+#         print(chan_data.shape)
+
+        with open(f'{self.input_file_stem}_dispersed.0000.raw', 'r+b') as f:
+
+            for j in range(self.blocks_to_read):
+
+                f.seek((self.start_block-1+j)*self.block_read_size, 0) 
+                f.seek(self.header_size, 1)
+#                 print(f.tell()-self.header_size)
+
+                block= xp.frombuffer(f.read(self.data_size), dtype=xp.int8).reshape(self.raw_params['num_chans'], self.chan_size)
+#                 plt.plot(xp.asnumpy(chan_data))
+#                 plt.show()
+
+                dispersed_chunk=block[chan_no].astype(int)+chan_data
+                plt.plot(xp.asnumpy(dispersed_chunk).astype(xp.int8))
+                plt.show()
+
+                f.seek(-self.data_size, 1)
+#                 print(f.tell()-self.header_size)
+                f.seek(chan_no*self.chan_size, 1)
+#                 print(f.tell()-self.header_size)
+
+                f.write(xp.array(dispersed_chunk, dtype=xp.int8).tobytes())
 
     
     def clear_cache(self):
@@ -86,132 +175,108 @@ class broadband(object):
 
     def chan_time_delay(self,x):
 
-        f_chan_arr= self.raw_params['fch1']+xp.linspace(0, self.obs_bw, self.raw_params['num_chans'] )
+        f_chan_arr= self.raw_params['fch1']+xp.linspace(0, self.obs_bw, self.raw_params['num_chans'], endpoint=False )
+        print(f_chan_arr)
         chan_centr=f_chan_arr+self.raw_params['chan_bw']/2.0
+        print(chan_centr)
         
-        time_delay= self.D*self.DM*(chan_centr[0]**-x - chan_centr**-x)
-        samples_to_shift=time_delay//self.raw_params['tbin']
-
+        time_delay= self.D*self.dm*(chan_centr[0]**-x - chan_centr**-x)
+        print(time_delay, len(time_delay))
+        samples_to_shift=np.ceil(time_delay/self.raw_params['tbin'])
+        print(samples_to_shift)
         return(samples_to_shift)
 
 
     def sample_shift(self,x=2 ):
+        
+        if x!=2:
+            
+            smear_x=self.calc_smear(x)
+            assert self.adjusted_pulse_time + smear_x<self.raw_params['obs_length'], f"smearing across band({smear}) for x= {x} exceeds length of file({self.raw_params['obs_length']}) "
 
         sample_shift=self.chan_time_delay(x)
+        gen_pulse=self.simulate_pulse(cmplx=False)
+        
 
         for i in range (self.raw_params['num_chans']):
             
-            gen_pulse=self.simulate_pulse(self.sim_len)
+            v=xp.roll(gen_pulse, -2*self.raw_params['num_pols']*int(sample_shift[i]))
             
-            v=xp.roll(gen_pulse, -2*slef.raw_params['num_pols']*int(sample_shift[i]))
-            self._write_(data, i)
+            self._write_(v, i)
             
 
 #dispersion by convolution 
 
-    def gaussian(self, x, mu, std, C):
-        denominator = xp.sqrt(2*xp.pi*std**2)
-        numerator = xp.exp(-1*(x - mu)**2/(2*std**2))
-        return C*(numerator/denominator)
-
-    def simulate_pulse(self, data_points):
-        x = xp.arange(width) #Span of Gaussian Pulse
-        gaussian_window1 = gaussian(x, int(width/4), 200, 50)
-        gaussian_window2 = gaussian(x, int(3*width/4), 200, 100)
-        gaussian_window = 1+ gaussian_window1 + gaussian_window2
-        scale_factors = xp.sqrt(2)*gaussian_window
-        noise = xp.random.normal(0,1,data_points)
-        for i in range(width):
-            if scale_factors[i] >= 1:
-                noise[(int)(data_points/2) + i] *= scale_factors[i]
-            else:
-                continue
-        real = noise
-        imag = noise
-        final_arr = xp.zeros(2*data_points)
-        final_arr[::2] = real
-        final_arr[1::2] = imag
-        ax1 = plt.subplot(211)
-        ax1.plot(xp.asnumpy(1+ gaussian_window1 + gaussian_window2))
-        ax2 = plt.subplot(212)
-        ax2.plot(xp.asnumpy(final_arr))
-        plt.show()
-        return final_arr
-
-        
     def impulse_length(self):
         
-        f_chan_arr= self.raw_params['fch1']+xp.linspace(0, self.obs_bw, self.raw_params['num_chans'] )
-        chan_centr=f_chan_arr+abs(self.raw_params['chan_bw'])/2.0
-        
-        for i in range(self.raw_params['num_chans']-1):
+        if self.raw_params['ascending']:
+            f_lower= self.raw_params['fch1']
+        else:
+            f_lower= self.raw_params['fch1'] + self.obs_bw
+#             reverse=True
             
-            t_d= self.D*self.DM*(chan_centr[i]**-2 - chan_centr[i+1]**-2)
         
-        imp_length=t_d//self.raw_params['tbin']
+        f_chan_start= f_lower + xp.linspace(0, np.abs(self.obs_bw), self.raw_params['num_chans'], endpoint=False)
+        print(f_chan_start)
+
+        t_d=self.D*self.dm*(f_chan_start**-2 - (f_chan_start+np.abs(self.raw_params['chan_bw']))**-2)
+    
+        print(t_d, len(t_d))
+
+        imp_length=np.ceil(t_d/self.raw_params['tbin'])
 
         n=xp.floor(xp.log(2*imp_length)/xp.log(2))
         nfft=2**n
         
-        print(imp_length, nfft)
-
-        return((imp_length), (nfft))
-
+        return(imp_length, nfft)
 
     def disperse(self):
         
-        f_coarse_dev=xp.linspace(0,self.obs_bw,self.raw_params['num_chans'], endpoint=False)
-        sim_len=blocks_to_read * data_size
+        f_coarse_dev=xp.linspace(0,np.abs(self.obs_bw),self.raw_params['num_chans'], endpoint=False)
+        print(f_coarse_dev)
 
-        imp_length, nfft=self.impulse_length()
+#         imp_length, nfft=self.impulse_length()
+#         print(imp_length, max(imp_length))
+        
+        data=xp.empty(self.chan_size,float)
+        
+        gen_pulse=self.simulate_pulse()
 
-        with open(f'{self.input_file_stem}.0000.raw', 'rb') as org:
-            with open(f'{self.input_file_stem}_dispersed.0000.raw', 'wb') as f:
+        qq=int(np.ceil(self.smear/self.raw_params['tbin']))
+        print(qq)
+        
+        for i in range (self.raw_params['num_chans']):
+#         for i in range (0,self.raw_params['num_chans'],20):
 
-                data=xp.empty(chan_size,xp.int8)
+            h=self.imp_res(f_coarse_dev[i], qq) 
 
-                for i in range (self.raw_params['num_chans']):
+#                 data_chan_cmplx=overlapSave(pulse_complex,h,nfft)
+            data_chan_cmplx=xp.convolve(gen_pulse,h, mode='same')
+#             ddd=xp.convolve(gen_pulse_complex,h, mode='full')
 
-                    h=self.imp_res(f_coarse_dev[i], imp_length[i]) 
-
-    #                 data_chan_cmplx=overlapSave(pulse_complex,h,nfft)
-                    data_chan_cmplx=xp.convolve(self.simulate_pulse(sim_len),h, mode='same')
-
-                    data[::2]=data_chan_cmplx.real
-                    data[1::2]=data_chan_cmplx.imag
-                    self._write_(data, i)
+            data[::2]=data_chan_cmplx.real
+            data[1::2]=data_chan_cmplx.imag
+            
+#             plt.plot(xp.asnumpy(xp.abs(data_chan_cmplx)), label=str(i))
+#             plt.legend(title=str(i))
+#             plt.plot(xp.asnumpy(xp.fft.fftshift(h)))
+#             plt.show()
+            self._write_(data, i,not self.raw_params['ascending'])
                     
     
-    def _write_(self,chan_data, chan_no):
-        
-        chan_data.reshape(slef.blocks_to_read, slef.chan_size)
-        
-        with open(f'{self.input_file_stem}.0000.raw', 'rb') as org:
-            with open(f'{self.input_file_stem}_dispersed.0000.raw', 'wb') as f:
-                
-                for j in range(self.blocks_to_read):
-
-                    f.seek((slef.start_block-1+j)*self.block_read_size, 0) 
-                    org.seek((self.start_block-1+j)*self.block_read_size, 0)
-
-                    f.seek(self.header_size, 1)
-                    org.seek(self.header_size, 1)
-
-                    block= xp.frombuffer(org.read(self.data_size), dtype=xp.int8).reshape(self.raw_params['num_chans'], self.chan_size)
-            #                        
-                    dispersed_chunk=block[chan_no]+chan_data[j]
-
-                    f.seek(chan_no*self.chan_size, 1)
-                    f.write(xp.array(dispersed_chunk, dtype=xp.int8).tobytes())
-
-                
-
     def imp_res(self, f_coarse_dev, imp_length):
-
-        f0=self.raw_params['fch1']
-        K=2 * xp.pi * self.D * self.dm / f0**2
         
-        fl = xp.linspace(0,self.raw_params['chan_bw'], imp_length, endpoint=False) + f_coarse_dev
+        if self.raw_params['ascending']:
+            f0= self.raw_params['fch1']
+        else:
+            f0= self.raw_params['fch1'] + self.obs_bw
+        
+#         print(f0)
+        
+        K=2 * xp.pi * self.D *1e6 * self.dm / f0**2
+        
+        fl = xp.linspace(0,np.abs(self.raw_params['chan_bw']), imp_length, endpoint=False) + f_coarse_dev
+#         print(fl)
         V=fl**2/(fl+f0)
 
         H= xp.exp(1j*K*V) 
